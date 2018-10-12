185261035f8c74cf6467856f967149d9dba98715
1810057 <1810057@bart.ca>
2018-10-12 14:01:46 -0400

    bombs starting

--- MineSweaper/app/src/main/java/ca/patrickrenaudbart/minesweaper/MainActivity.java
+++ MineSweaper/app/src/main/java/ca/patrickrenaudbart/minesweaper/MainActivity.java
@@ -1,23 +1,40 @@
 package ca.patrickrenaudbart.minesweaper;
 
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.GridLayout;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
 
     public int[] data = new int[100];
+    public static final int FLAG = 2;
     public static final int MINE = 1;
     public static final int EXPOSED = 0;
+    GridLayout grid;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        GridLayout grid = findViewById(R.id.grid);
+        grid = findViewById(R.id.grid);
+
+        // This is to get random bombs location
+        ArrayList<Integer> bombs = new ArrayList<Integer>(10);
+        for (int i = 0; i < 10; i++)
+        {
+            int number = GetRandomNumbers(0,100);
+            bombs.add(new Integer(number));
+        }
+        SetBombs(bombs);
+
+        // This is to access OnCellClick on every Buttons
         for(int i = 0; i < 10; i++)
         {
             for (int j = 0; j < 10; j++)
@@ -38,8 +55,41 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    public int GetRandomNumbers(int min, int max)
+    {
+
+        return new Random().nextInt((max - min) + 1) + min;
+    }
+
+    public void SetBombs(ArrayList<Integer> bombs)
+    {
+
+        for(int i = 0; i < bombs.size(); i++)
+        {
+            TurnBitOn(data[bombs.get(i)], 1);
+        }
+    }
+
+
     public void OnCellClicked(int x, int y)
     {
+        int index = x + y * 10;
+        Button button = (Button) grid.getChildAt(index);
+
+        if(IsBitOn(index, FLAG))
+        {
+            button.setBackgroundResource(R.drawable.ic_button_flag);
+        }
+        else if(IsBitOn(index, MINE))
+        {
+            // GameOver
+            button.setBackgroundResource((R.drawable.ic_button_bombblow));
+        }
+        else
+        {
+            button.setBackgroundResource(R.drawable.ic_button_1);
+        }
+
 
         // Reveal(x, y);
 
